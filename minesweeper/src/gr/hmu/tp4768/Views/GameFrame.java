9c80af604ed8830435214c73da40a5f1921998ab
kek-sec <georgepetrakis99@gmail.com>
2022-01-09 21:45:22 +0200

    Create GameFrame.java

---
+++ minesweeper/src/gr/hmu/tp4768/Views/GameFrame.java
@@ -0,0 +1,14 @@
+package gr.hmu.tp4768.Views;
+/**
+ * @author tp4768
+ * 
+ * GameFrame extends JFrame and creates the main window of the game.
+ * It consists of , a game tool bar, a board and a status bar.
+ * 
+ */
+
+import javax.swing.JFrame;
+
+public class GameFrame extends JFrame{
+    
+}
