b16372b5f1ade6bbd6ebcef9ebbad180a9795a82
kek-sec <georgepetrakis99@gmail.com>
2022-01-09 15:34:34 +0200

    Create GameModeMenuListener.java

---
+++ minesweeper/src/gr/hmu/tp4768/Listeners/Menus/GameModeMenuListener.java
@@ -0,0 +1,19 @@
+package gr.hmu.tp4768.Listeners.Menus;
+/**
+ * @author tp4768
+ * 
+ * Listener for the game mode menu.
+ */
+public class GameModeMenuListener implements java.awt.event.ActionListener {
+
+    /**
+     * @param e
+     * @return
+     * @see java.awt.event.ActionListener#actionPerformed(java.awt.event.ActionEvent)
+     */
+    @Override
+    public void actionPerformed(java.awt.event.ActionEvent e) {
+        // TODO Auto-generated method stub
+        System.out.println("GameModeMenuListener");
+    }
+}
