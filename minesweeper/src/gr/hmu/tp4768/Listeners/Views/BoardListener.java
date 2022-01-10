822c8167924321419601f3776ee110991a8e7366
kek-sec <georgepetrakis99@gmail.com>
2022-01-10 20:19:52 +0200

    Create BoardListener.java

---
+++ minesweeper/src/gr/hmu/tp4768/Listeners/Views/BoardListener.java
@@ -0,0 +1,13 @@
+package gr.hmu.tp4768.Listeners.Views;
+/**
+ * @author tp4768
+ * 
+ * ActionListeners for the board buttons
+ * Every board button is a minesweeper square. defined in Square.java
+ * This class contains the logic for the minesweeper squares.
+ */
+public class BoardListener {
+    
+    
+
+}
