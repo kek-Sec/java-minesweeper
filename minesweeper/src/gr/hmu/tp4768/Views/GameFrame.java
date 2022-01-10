022d88baceab946c2e82a9e1c889b327051ed153
kek-sec <georgepetrakis99@gmail.com>
2022-01-10 20:08:32 +0200

    Update GameFrame.java

--- minesweeper/src/gr/hmu/tp4768/Views/GameFrame.java
+++ minesweeper/src/gr/hmu/tp4768/Views/GameFrame.java
@@ -1,6 +1,12 @@
 package gr.hmu.tp4768.Views;
 
 import javax.swing.JFrame;
+
+import gr.hmu.tp4768.Menus.GameToolBar;
+import gr.hmu.tp4768.Menus.StatusBar;
+import gr.hmu.tp4768.Models.GameMode;
+import java.awt.BorderLayout;
+import java.awt.Dimension;
 /**
  * @author tp4768
  * 
@@ -8,7 +14,35 @@ import javax.swing.JFrame;
  * It consists of , a game tool bar, a board and a status bar.
  * 
  */
-
 public class GameFrame extends JFrame{
     
+    private GameMode _gameMode;
+    /**
+     * constructor
+     */
+    public GameFrame(GameMode gameMode){
+        _gameMode = gameMode;
+        setTitle("Minesweeper");
+        initComponents();
+        setSize(new Dimension(500, 500));
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+    }
+
+    /**
+     * Function to initialize the components of the game frame.
+     * At the the top of the frame is the game tool bar.
+     * At the bottom of the frame is the status bar.
+     * At the center of the frame is the board.
+     */
+    public void initComponents(){
+        GameToolBar gameToolBar = new GameToolBar();
+        Board board = new Board(this._gameMode);
+        StatusBar statusBar = new StatusBar(99,99);
+        add(gameToolBar, BorderLayout.NORTH);
+        add(board, BorderLayout.CENTER);
+        add(statusBar, BorderLayout.SOUTH);
+    }
 }
