93ac260af98ddc4623886ab08ea5182d51d5e48e
kek-Sec <georgepetrakis99@gmail.com>
2022-02-07 11:21:26 +0200

    Update App.java

--- minesweeper/src/gr/hmu/tp4768/App.java
+++ minesweeper/src/gr/hmu/tp4768/App.java
@@ -1,11 +1,10 @@
 package gr.hmu.tp4768;
 
-import gr.hmu.tp4768.Menus.GameModeMenu;
 import gr.hmu.tp4768.Views.SplashScreen;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //Display the splash screen
+        // Display the splash screen
         SplashScreen splashScreen = new SplashScreen();
     }
 }
