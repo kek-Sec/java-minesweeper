a8bb8157cfa22e1a38d259c94ffeabd486f70730
kek-Sec <georgepetrakis99@gmail.com>
2022-02-07 11:46:23 +0200

    Update GameFrame.java

--- minesweeper/src/gr/hmu/tp4768/Views/GameFrame.java
+++ minesweeper/src/gr/hmu/tp4768/Views/GameFrame.java
@@ -7,24 +7,27 @@ import gr.hmu.tp4768.Menus.StatusBar;
 import gr.hmu.tp4768.Models.GameMode;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+
 /**
  * @author tp4768
- * 
- * GameFrame extends JFrame and creates the main window of the game.
- * It consists of , a game tool bar, a board and a status bar.
- * 
+ *
+ *         GameFrame extends JFrame and creates the main window of the game.
+ *         It consists of , a game tool bar, a board and a status bar.
+ *
  */
-public class GameFrame extends JFrame{
-    
+public class GameFrame extends JFrame {
+
     private GameMode _gameMode;
+
     /**
      * constructor
      */
-    public GameFrame(GameMode gameMode){
+    public GameFrame(GameMode gameMode) {
         _gameMode = gameMode;
         setTitle("Minesweeper");
         initComponents();
-        setSize(new Dimension(500, 500));
+        // set size based on difficulty
+        setSize(new Dimension(gameMode.getNumberOfRows() * 60, gameMode.getNumberOfRows() * 40 + 100));
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
@@ -37,10 +40,10 @@ public class GameFrame extends JFrame{
      * At the bottom of the frame is the status bar.
      * At the center of the frame is the board.
      */
-    public void initComponents(){
+    public void initComponents() {
         GameToolBar gameToolBar = new GameToolBar(this);
         Board board = new Board(this._gameMode);
-        StatusBar statusBar = new StatusBar(this._gameMode.getNumberOfMines(),this._gameMode.getNumberOfFlags());
+        StatusBar statusBar = new StatusBar(this._gameMode.getNumberOfMines(), this._gameMode.getNumberOfFlags());
         add(gameToolBar, BorderLayout.NORTH);
         add(board, BorderLayout.CENTER);
         add(statusBar, BorderLayout.SOUTH);
