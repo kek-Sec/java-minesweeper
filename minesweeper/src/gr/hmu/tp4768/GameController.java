96772f31a18ff26b85aae96bfa23ccab42fa5714
kek-Sec <georgepetrakis99@gmail.com>
2022-02-07 12:35:02 +0200

    Update GameController.java

--- minesweeper/src/gr/hmu/tp4768/GameController.java
+++ minesweeper/src/gr/hmu/tp4768/GameController.java
@@ -1,13 +1,12 @@
 package gr.hmu.tp4768;
 
 import gr.hmu.tp4768.Models.GameMode;
-import gr.hmu.tp4768.Views.Board;
 import gr.hmu.tp4768.Views.GameFrame;
 
 /**
  * @author tp4768
- * 
- * Game controller is a static class that contains the game variables.
+ *
+ *         Game controller is a static class that contains the game variables.
  */
 public class GameController {
     private static GameMode gameMode;
@@ -17,9 +16,9 @@ public class GameController {
      */
     public static void setGameMode(GameMode gameMode) {
         GameController.gameMode = gameMode;
-        //print that the game mode has been changed and the new game mode
+        // print that the game mode has been changed and the new game mode
         System.out.println("Game mode changed to " + gameMode.getDifficulty());
-        //display Board
+        // display Board
         GameFrame gameFrame = new GameFrame(gameMode);
     }
 
