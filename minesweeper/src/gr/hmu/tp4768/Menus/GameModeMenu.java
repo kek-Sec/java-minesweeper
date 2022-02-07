e48b8550663731cee5495ad552507b888fc2f5d3
kek-Sec <georgepetrakis99@gmail.com>
2022-02-07 11:21:36 +0200

    Update GameModeMenu.java

--- minesweeper/src/gr/hmu/tp4768/Menus/GameModeMenu.java
+++ minesweeper/src/gr/hmu/tp4768/Menus/GameModeMenu.java
@@ -6,12 +6,12 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import gr.hmu.tp4768.Listeners.Menus.GameModeMenuListener;
-import gr.hmu.tp4768.Views.SplashScreen;
 
 /**
  * @author tp4768
- * 
- * class that extends JPanel, and displays the game mode menu. Based on GameModes enum.
+ *
+ *         class that extends JPanel, and displays the game mode menu. Based on
+ *         GameModes enum.
  */
 public class GameModeMenu extends JPanel {
 
@@ -20,13 +20,14 @@ public class GameModeMenu extends JPanel {
     /**
      * Menu buttons
      */
-     JButton _EasyButton, _MediumButton, _HardButton;
-     //label
-     JLabel _Label;
+    JButton _EasyButton, _MediumButton, _HardButton;
+    // label
+    JLabel _Label;
 
     /**
      * Creates new
      * form GameModeMenu
+     * 
      * @param gameModeMenuListener
      */
     public GameModeMenu(JFrame ParentFrame) {
@@ -37,15 +38,17 @@ public class GameModeMenu extends JPanel {
 
     /**
      * Function to initialize the components of the game mode menu.
-     * Action Handling is done in a seperate class but keep in mind that we pass the parent JFrame to the action handler
-     * in order to be able to close the parent frame. when the user clicks on a button.
+     * Action Handling is done in a seperate class but keep in mind that we pass the
+     * parent JFrame to the action handler
+     * in order to be able to close the parent frame. when the user clicks on a
+     * button.
      */
-    public void initComponents(){
+    public void initComponents() {
         _MenuListener = new GameModeMenuListener();
-        //add label
+        // add label
         _Label = new JLabel("Choose Game Mode      ");
         this.add(_Label);
-        //add buttons below label
+        // add buttons below label
         _EasyButton = new JButton("Easy");
         _EasyButton.addActionListener(_MenuListener.actionPerformedEasy(this._Parent));
         this.add(_EasyButton);
